323b9ca443a58db93ea6ff82f760eb0316ae3a78
incoCodes <71338998+incoCodes@users.noreply.github.com>
2021-09-27 19:22:52 +0300

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothTime = 0.3f;
+    private Vector3 _Velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref _Velocity, smoothTime);
+    }
+}
