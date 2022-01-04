db944208f230c18682d8eeae869a2bf422dcc3f6
incoCodes <71338998+incoCodes@users.noreply.github.com>
2022-01-04 20:45:11 +0300

    Update Hitbox.cs

--- Hitbox.cs
+++ Hitbox.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Hitbox : MonoBehaviour
 {
+    // Assign an game object visible in the editior to the player object
+    // This is so that we can call this object later and modify however we like with this script
     public GameObject player;
 
+// This tells the program that if the enemy's collider collides with the player collider then kill or destory the player game object from the scene.
+// This adds some difficulty in the game as the player can die from the enemy but can't kill the enemy
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "enemy")
